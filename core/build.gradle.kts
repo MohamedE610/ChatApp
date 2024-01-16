c8f391692467b9ae6da9e019164e0312f9cf799b
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-01-16 22:33:17 +0200

    add project dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -33,11 +36,66 @@ android {
 }
 
 dependencies {
+    api("androidx.core:core-ktx:1.12.0")
+    api("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    api("androidx.activity:activity-compose:1.8.2")
+    api(platform("androidx.compose:compose-bom:2023.08.00"))
+    api("androidx.compose.ui:ui")
+    api("androidx.compose.ui:ui-graphics")
+    api("androidx.compose.ui:ui-tooling-preview")
+    api("androidx.compose.material3:material3")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    //test
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //mockito
+    testImplementation("org.mockito:mockito-core:5.7.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+
+    //coroutines
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //viewModel
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    //room
+    api("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+
+    //retrofit2
+    api("com.squareup.retrofit2:retrofit:2.9.0")
+    api("com.squareup.retrofit2:converter-gson:2.9.0")
+    api("com.squareup.okhttp3:logging-interceptor:4.9.2")
+
+    //hilt
+    api("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+    api("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+
+    //work
+    api("androidx.work:work-runtime-ktx:2.9.0")
+    api("androidx.work:work-rxjava3:2.9.0")
+
+    //fragmentKTX
+    api("androidx.fragment:fragment-ktx:1.6.2")
+
+    //glide
+    api("com.github.bumptech.glide:glide:4.13.2")
+
+    //facebook shimmer
+    api("com.facebook.shimmer:shimmer:0.5.0")
+
+    //chucker
+    debugImplementation("com.github.chuckerteam.chucker:library:3.5.2")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:3.5.2")
 }
