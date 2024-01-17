1599a186cd096a0dcca8c4fe9868e25366f1473c
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-01-17 02:01:17 +0200

    add test dependencies in app and chat module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,17 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":features:chat"))
 
+    //test
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //mockito
+    testImplementation("org.mockito:mockito-core:5.7.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
 }
