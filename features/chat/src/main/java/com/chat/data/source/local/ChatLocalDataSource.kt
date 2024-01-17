0912deeed7dcbae2189b3d233fe4d49e7e3dd54f
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-01-17 15:04:48 +0200

    implement ChatLocalDataSource in data layer for chat module

---
+++ features/chat/src/main/java/com/chat/data/source/local/ChatLocalDataSource.kt
@@ -0,0 +1,23 @@
+package com.chat.data.source.local
+
+import com.core.extension.emitFlow
+import com.core.room.dao.MessagesDao
+import com.core.room.entity.MessageEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ChatLocalDataSource @Inject constructor(
+    private val messageDao: MessagesDao
+) {
+    suspend fun loadHistory(): Flow<List<MessageEntity>> {
+        return emitFlow { messageDao.getMessages() }
+    }
+
+    suspend fun saveMessage(msg: MessageEntity) {
+        messageDao.saveMessage(msg)
+    }
+
+    suspend fun clearCache() {
+        messageDao.deleteAll()
+    }
+}
